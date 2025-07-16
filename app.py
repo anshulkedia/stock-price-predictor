eb6f071d862d80445ac27670cbb7144008b9cc16
Anshul Kedia <anshulkedia@Anshuls-MacBook-Pro-2.local>
2025-07-16 17:46:28 +0530

    fixing port issue

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def predict():
     return jsonify({'predictions': pred.tolist()})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env var
+    port = int(os.environ.get("PORT", 5001))  # Render sets PORT env var
     app.run(host='0.0.0.0', port=port)
